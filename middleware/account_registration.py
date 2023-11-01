d843ac3d8304140b4c58a7b01540cc347771833a
clarencesarmiento <sarmiento.clarence1020@gmail.com>
2023-11-01 17:27:59 +0800

    Account Validation and Test for Registration

---
+++ middleware/account_registration.py
@@ -0,0 +1,89 @@
+import string
+from email_validator import validate_email, EmailNotValidError
+from secrets import token_hex
+import re
+import hashlib
+
+
+class AccountRegistration:
+    def __init__(self):
+        self._fullname = None
+        self._email = None
+        self._password = None
+
+    @property
+    def fullname(self) -> str:
+        return self._fullname
+
+    @fullname.setter
+    def fullname(self, fullname: str):
+        if fullname is None or fullname == '':
+            raise ValueError('Fullname field is Required.')
+        elif not re.match(r'^[A-Za-z]+(?: [A-Za-z]+)*$', fullname):
+            raise ValueError('Fullname has invalid character.')
+
+        self._fullname = fullname
+
+    @property
+    def email(self) -> str:
+        return self._email
+
+    @email.setter
+    def email(self, email: str):
+        if email is None or email == '':
+            raise ValueError('Email field is Required.')
+        try:
+            email_info = validate_email(email, check_deliverability=True)
+            email = email_info.normalized
+        except EmailNotValidError as e:
+            raise ValueError(e)
+
+        self._email = email
+
+    @property
+    def password(self) -> str:
+        return self._password
+
+    @password.setter
+    def password(self, password: str):
+        # Define criteria for password strength
+        length_criteria = 8
+
+        # Check the password entry
+        if password is None or password == '':
+            raise ValueError('Password field is Required.')
+        # Check the length of the password
+        elif len(password) < length_criteria:
+            raise ValueError(f'Password should be at least {length_criteria} character long.')
+        # Check for at least one uppercase letter
+        elif not any(char.isupper() for char in password):
+            raise ValueError('Password should contain at least one uppercase letter.')
+        # Check for at least one lowercase letter
+        elif not any(char.lower() for char in password):
+            raise ValueError('Password should contain at least one lowercase letter.')
+        # Check for at least one digit
+        elif not any(char.isdigit() for char in password):
+            raise ValueError('Password should contain at least one digit.')
+        # Check for at least one special character
+        elif not any(char in string.punctuation for char in password):
+            raise ValueError('Password should contain at least one special character.')
+
+        # Generate random salt
+        salt = token_hex(16)
+
+        # Combine the salt and the password
+        salted_password = salt.encode() + password.encode()
+
+        # Hash the salted password
+        hashed_password = hashlib.sha256(salted_password).hexdigest()
+
+        self._password = salt + '$' + hashed_password
+
+
+if __name__ == '__main__':
+    obj = AccountRegistration()
+    obj.fullname = 'John Doe'
+    obj.email = 'johndoe@gmail.com'
+    obj.password = 'Johndoe@122'
+
+    print(f'Fullname: {obj.fullname}, Email: {obj.email}, Password: {obj.password}')
---
+++ middleware/test_account_registration.py
@@ -0,0 +1,94 @@
+from middleware.account_registration import AccountRegistration
+import pytest
+
+
+def test_init():
+    acc_registration = AccountRegistration()
+
+
+def test_set_valid_values_with_setter():
+    acc_registration = AccountRegistration()
+    acc_registration.fullname = 'John Doe'
+    acc_registration.email = 'johndoe@gmail.com'
+    acc_registration.password = 'JohnDoe@112'
+
+    assert acc_registration.fullname == 'John Doe'
+    assert acc_registration.email == 'johndoe@gmail.com'
+    assert acc_registration != 'JohnDoe@112'
+
+
+def test_fullname():
+    acc_registration = AccountRegistration()
+
+    valid_test_input = ['John Doe', 'Jennifer Smith', 'Samantha Grace Davis']
+    for name in valid_test_input:
+        acc_registration.fullname = name
+        assert acc_registration.fullname == name
+
+    require_test_input = ['', None]
+    with pytest.raises(ValueError) as e:
+        for test in require_test_input:
+            acc_registration.fullname = test
+            assert str(e.value) == 'Fullname field is Required.'
+
+    invalid_test_input = ['John Do3', 'Jennifer@Smith', 'Samantha!!Grace_Davis']
+    with pytest.raises(ValueError) as e:
+        for test in invalid_test_input:
+            acc_registration.fullname = test
+            assert str(e.value) == 'Fullname has invalid character.'
+
+
+def test_email():
+    acc_registration = AccountRegistration()
+
+    valid_test_input = ['johndoe@gmail.com', 'Jennifer.Smith@yahoo.com', 'davis_samGrace@me.org']
+    for email in valid_test_input:
+        acc_registration.email = email
+        assert acc_registration.email == email
+
+    require_test_input = ['', None]
+    with pytest.raises(ValueError) as e:
+        for test in require_test_input:
+            acc_registration.email = test
+            assert str(e.value) == 'Email field is Required.'
+
+    invalid_test_input = ['johndoe.gmail.com', 'Jennifer.Smith@yah.com', 'davis_samGrace@me.o']
+    with pytest.raises(ValueError) as e:
+        for test in invalid_test_input:
+            acc_registration.email = test
+            assert str(e.value) == e
+
+
+def test_password():
+    acc_registration = AccountRegistration()
+
+    valid_test_input = ['Johndoe@112', 'Jennifer.Smith_241', 'Samantha_grace@1422']
+    for password in valid_test_input:
+        acc_registration.password = password
+        assert acc_registration.password != password
+
+    require_test_input = ['', None]
+    with pytest.raises(ValueError) as e:
+        for test in require_test_input:
+            acc_registration.password = test
+            assert str(e.value) == 'Password field is Required.'
+
+    with pytest.raises(ValueError) as e:
+        acc_registration.password = 'John!12'
+        assert str(e.value) == 'Password should be at least 8 character long.'
+
+    with pytest.raises(ValueError) as e:
+        acc_registration.password = 'john!122'
+        assert str(e.value) == 'Password should contain at least one uppercase letter.'
+
+    with pytest.raises(ValueError) as e:
+        acc_registration.password = 'JOHND122'
+        assert str(e.value) == 'Password should contain at least one lowercase letter.'
+
+    with pytest.raises(ValueError) as e:
+        acc_registration.password = 'JohnDoe!'
+        assert str(e.value) == 'Password should contain at least one digit.'
+
+    with pytest.raises(ValueError) as e:
+        acc_registration.password = 'JohnDoe122'
+        assert str(e.value) == 'Password should contain at least one special character.'
