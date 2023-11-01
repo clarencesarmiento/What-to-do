c0d9a406e25f7a6613a2d8f736f2043f10e7ac7d
clarencesarmiento <sarmiento.clarence1020@gmail.com>
2023-11-01 17:27:12 +0800

    Created Database Connection

---
+++ backend/db_connection.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+
+def connect_to_database():
+    conn = None
+    try:
+        conn = sqlite3.connect('../backend/AppDatabase.db')
+        cursor = conn.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS accounts "
+                       "(fullname TEXT NOT NULL, email TEXT NOT NULL, password TEXT NOT NULL);")
+        cursor.close()
+    except sqlite3.Error as e:
+        print(f'SQLite Connection Error: {e}')
+    finally:
+        return conn
+
+
+if __name__ == '__main__':
+    connection = connect_to_database()
+    connection.close()
